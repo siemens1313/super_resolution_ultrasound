658393ed2e6ef63b5f536134498cd980d5116b3e
boran zhou <boranzhou1313@gmail.com>
2019-01-24 14:38:54 -0600

    my first commit

---
+++ dcgan_ILD.py
@@ -0,0 +1,414 @@
+from __future__ import division, print_function, absolute_import
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Reshape
+from keras.layers.core import Activation
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import UpSampling2D
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.core import Flatten
+from keras.optimizers import SGD
+from keras.datasets import mnist
+import numpy as np
+from PIL import Image
+import argparse
+import math
+from keras.utils import np_utils
+import os
+from numpy import *
+from sklearn.utils import shuffle
+from sklearn.cross_validation import train_test_split
+
+
+
+def identity_block(X, f, filters, stage, block):
+    """
+    Implementation of the identity block as defined in Figure 3
+    
+    Arguments:
+    X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    f -- integer, specifying the shape of the middle CONV's window for the main path
+    filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    stage -- integer, used to name the layers, depending on their position in the network
+    block -- string/character, used to name the layers, depending on their position in the network
+    
+    Returns:
+    X -- output of the identity block, tensor of shape (n_H, n_W, n_C)
+    """
+    
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    
+    # Retrieve Filters
+    F1, F2, F3 = filters
+    
+    # Save the input value. You'll need this later to add back to the main path. 
+    X_shortcut = X
+    
+    # First component of main path
+    X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
+    
+    ### START CODE HERE ###
+    
+    # Second component of main path (≈3 lines)
+    X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+    # Third component of main path (≈2 lines)
+    X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
+
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
+    X =  Add()([X_shortcut, X])  # equivalent to added = keras.layers.add([x1, x2])
+    X = Activation('relu')(X)
+    
+    ### END CODE HERE ###
+    
+    return X
+
+# GRADED FUNCTION: convolutional_block
+
+def convolutional_block(X, f, filters, stage, block, s = 2):
+    """
+    Implementation of the convolutional block as defined in Figure 4
+    
+    Arguments:
+    X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    f -- integer, specifying the shape of the middle CONV's window for the main path
+    filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    stage -- integer, used to name the layers, depending on their position in the network
+    block -- string/character, used to name the layers, depending on their position in the network
+    s -- Integer, specifying the stride to be used
+    
+    Returns:
+    X -- output of the convolutional block, tensor of shape (n_H, n_W, n_C)
+    """
+    
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    
+    # Retrieve Filters
+    F1, F2, F3 = filters
+    
+    # Save the input value
+    X_shortcut = X
+
+
+    ##### MAIN PATH #####
+    # First component of main path 
+    X = Conv2D(F1, kernel_size=(1, 1), strides = (s,s),padding = 'valid', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
+    
+    ### START CODE HERE ###
+
+    # Second component of main path (≈3 lines)
+    X = Conv2D(F2,kernel_size= (f, f), strides = (1,1),padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+    # Third component of main path (≈2 lines)
+    X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
+
+    ##### SHORTCUT PATH #### (≈2 lines)
+    X_shortcut =  Conv2D(filters = F3, kernel_size = (1, 1), strides = (s,s), padding = 'valid', name = conv_name_base + '1', kernel_initializer = glorot_uniform(seed=0))(X_shortcut)
+    X_shortcut = BatchNormalization(axis = 3, name = bn_name_base + '1')(X_shortcut)
+
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
+    X =  Add()([X_shortcut, X])  # equivalent to added = keras.layers.add([x1, x2])
+    X = Activation('relu')(X)
+    
+    ### END CODE HERE ###
+    
+    return X
+
+
+# GRADED FUNCTION: ResNet50
+
+def ResNet50(input_shape = (64, 64, 3), classes = 6):
+    """
+    Implementation of the popular ResNet50 the following architecture:
+    CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> CONVBLOCK -> IDBLOCK*2 -> CONVBLOCK -> IDBLOCK*3
+    -> CONVBLOCK -> IDBLOCK*5 -> CONVBLOCK -> IDBLOCK*2 -> AVGPOOL -> TOPLAYER
+
+    Arguments:
+    input_shape -- shape of the images of the dataset
+    classes -- integer, number of classes
+
+    Returns:
+    model -- a Model() instance in Keras
+    """
+    
+    # Define the input as a tensor with shape input_shape
+    X_input = Input(input_shape)
+
+    
+    # Zero-Padding
+    X = ZeroPadding2D((3, 3))(X_input)
+    
+    # Stage 1
+    X = Conv2D(64, (7, 7), strides = (2, 2), name = 'conv1', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = 'bn_conv1')(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+    # Stage 2
+    X = convolutional_block(X, f = 3, filters = [64, 64, 256], stage = 2, block='a', s = 1)
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='b')
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='c')
+
+    ### START CODE HERE ###
+
+    # Stage 3 (≈4 lines)
+    X = convolutional_block(X, f = 3, filters = [128,128,512], stage = 3, block='a', s = 2)
+    X = identity_block(X, 3, [128,128,512], stage=3, block='b')
+    X = identity_block(X, 3, [128,128,512], stage=3, block='c')
+    X = identity_block(X, 3, [128,128,512], stage=3, block='d')
+
+    # Stage 4 (≈6 lines)
+    X = convolutional_block(X, f = 3, filters = [256, 256, 1024], stage = 4, block='a', s = 2)
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='b')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='c')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='d')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
+
+    # Stage 5 (≈3 lines)
+    X = convolutional_block(X, f = 3, filters = [512, 512, 2048], stage = 5, block='a', s = 2)
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='b')
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='c')
+
+    # AVGPOOL (≈1 line). Use "X = AveragePooling2D(...)(X)"
+    X = AveragePooling2D(pool_size=(2, 2), strides=None, padding='valid',name='avg_pool' )(X)
+    
+    ### END CODE HERE ###
+
+    # output layer
+    X = Flatten()(X)
+    X = Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer = glorot_uniform(seed=0))(X)
+    
+    
+    # Create model
+    model = Model(inputs = X_input, outputs = X, name='ResNet50')
+
+    return model
+
+
+def generator_model():
+    model = Sequential()
+    model.add(Dense(input_dim=100, output_dim=1024))
+    model.add(Activation('tanh'))
+    # model.add(Dense(128*7*7))
+    model.add(Dense(128*64*64))
+    model.add(BatchNormalization())
+    model.add(Activation('tanh'))
+    # model.add(Reshape((7, 7, 128), input_shape=(128*7*7,)))
+    model.add(Reshape((64, 64, 128), input_shape=(128*64*64,)))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Conv2D(64, (5, 5), padding='same'))
+    model.add(Activation('tanh'))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Conv2D(1, (5, 5), padding='same'))
+    model.add(Activation('tanh'))
+    return model
+
+
+def discriminator_model():
+    model = Sequential()
+    model.add(
+            Conv2D(64, (5, 5),
+            padding='same',
+            # input_shape=(28, 28, 1))
+            input_shape=(256, 256, 1))
+            )
+    model.add(Activation('tanh'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, (5, 5)))
+    model.add(Activation('tanh'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(1024))
+    model.add(Activation('tanh'))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    return model
+
+
+def generator_containing_discriminator(g, d):
+    model = Sequential()
+    model.add(g)
+    d.trainable = False
+    model.add(d)
+    return model
+
+
+def combine_images(generated_images):
+    num = generated_images.shape[0]
+    width = int(math.sqrt(num))
+    height = int(math.ceil(float(num)/width))
+    shape = generated_images.shape[1:3]
+    image = np.zeros((height*shape[0], width*shape[1]),
+                     dtype=generated_images.dtype)
+    for index, img in enumerate(generated_images):
+        i = int(index/width)
+        j = index % width
+        image[i*shape[0]:(i+1)*shape[0], j*shape[1]:(j+1)*shape[1]] = \
+            img[:, :, 0]
+    return image # 2d matrix
+
+
+def train(BATCH_SIZE):
+
+    # input image dimensions
+    img_rows, img_cols = 256, 256
+
+    # number of channels
+    img_channels = 3
+
+    #%%
+    #  data
+
+    path1 = 'H:/lung/CNN/posterior/images'    #path of folder of images    
+    path2 = 'H:/git/keras-dcgan/resized_256'  #path of folder to save images    
+
+    listing = os.listdir(path1) 
+    num_samples=size(listing)
+    # print (num_samples)
+
+    for file in listing:
+        im = Image.open(path1 + '/' + file)   
+        img = im.resize((img_rows,img_cols))
+        #img = array(img).reshape(1,img_rows,img_cols,1)
+        gray = img.convert('L')
+                    #need to do some more processing here           
+        gray.save(path2 +'/' +  file, "JPEG")
+        img.save(path2 +'/' +  file, "JPEG")
+
+    imlist = os.listdir(path2)
+
+    #im1 = array(Image.open('resized' + '/'+ imlist[0])) # open one image to get size
+    im1 = array(Image.open(path2 + '/'+ imlist[0])) # open one image to get size
+    m,n = im1.shape[0:2] # get the size of the images
+    imnbr = len(imlist) # get the number of images
+
+    # create matrix to store all flattened images
+    #immatrix = array([array(Image.open('resized'+ '/' + im2)).flatten()
+                  #for im2 in imlist],'f')
+    immatrix = array([array(Image.open(path2 + '/' + im2)).flatten()
+                  for im2 in imlist],'f')   
+    immatrix=immatrix.reshape(imnbr,img_rows,img_cols,1)        
+    # label=np.ones((num_samples,),dtype = int)
+    # label[0:555]=0
+    # label[556:1363]=1
+    # label[1364:]=2
+
+
+    data = shuffle(immatrix, random_state=2)
+    train_data = data
+
+    # #img=immatrix[167].reshape(img_rows,img_cols)
+
+    # # plt.imshow(img)
+    # # plt.imshow(img,cmap='gray')
+    # print (train_data[0].shape)
+    # print (train_data[1].shape)
+
+    #%%
+    (X, Y) = (train_data[0],train_data[1])
+    # STEP 1: split X and y into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=4)
+    # (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    X_train = (X_train.astype(np.float32) - 127.5)/127.5
+    X_train = X_train[:, :, :, None]
+    X_test = X_test[:, :, :, None]
+    # X_train = X_train.reshape((X_train.shape, 1) + X_train.shape[1:])
+    d = discriminator_model()
+    g = generator_model()
+    d_on_g = generator_containing_discriminator(g, d)
+    d_optim = adam(lr=0.0001, momentum=0.9, nesterov=True)
+    g_optim = adam(lr=0.0001, momentum=0.9, nesterov=True)
+    g.compile(loss='binary_crossentropy', optimizer="adam")
+    d_on_g.compile(loss='binary_crossentropy', optimizer=g_optim)
+    d.trainable = True
+    d.compile(loss='binary_crossentropy', optimizer=d_optim)
+    for epoch in range(10):
+        print("Epoch is", epoch)
+        print("Number of batches", int(X_train.shape[0]/BATCH_SIZE))
+        for index in range(int(X_train.shape[0]/BATCH_SIZE)):
+            noise = np.random.uniform(-1, 1, size=(BATCH_SIZE, 100))
+            image_batch = X_train[index*BATCH_SIZE:(index+1)*BATCH_SIZE]
+            image_batch = image_batch.reshape(128,img_rows,img_cols,1) 
+            print(shape(image_batch))
+            generated_images = g.predict(noise, verbose=0)
+            print(shape(generated_images))
+            if index % 20 == 0:
+                image = combine_images(generated_images)
+                image = image*127.5+127.5
+                Image.fromarray(image.astype(np.uint8)).save(
+                    str(epoch)+"_"+str(index)+".png")
+                # fromarray: creates an image memory from an object exporting the array interface
+            X = np.concatenate((image_batch, generated_images))
+            y = [1] * BATCH_SIZE + [0] * BATCH_SIZE
+            d_loss = d.train_on_batch(X, y)
+            print("batch %d d_loss : %f" % (index, d_loss))
+            noise = np.random.uniform(-1, 1, (BATCH_SIZE, 100))
+            d.trainable = False
+            g_loss = d_on_g.train_on_batch(noise, [1] * BATCH_SIZE)
+            d.trainable = True
+            print("batch %d g_loss : %f" % (index, g_loss))
+            if index % 10 == 9:
+                g.save_weights('generator', True)
+                d.save_weights('discriminator', True)
+
+
+def generate(BATCH_SIZE, nice=False):
+    g = generator_model()
+    g.compile(loss='binary_crossentropy', optimizer="SGD")
+    g.load_weights('generator')
+    if nice:
+        d = discriminator_model()
+        d.compile(loss='binary_crossentropy', optimizer="SGD")
+        d.load_weights('discriminator')
+        noise = np.random.uniform(-1, 1, (BATCH_SIZE*20, 100))
+        generated_images = g.predict(noise, verbose=1)
+        d_pret = d.predict(generated_images, verbose=1)
+        index = np.arange(0, BATCH_SIZE*20)
+        index.resize((BATCH_SIZE*20, 1))
+        pre_with_index = list(np.append(d_pret, index, axis=1))
+        pre_with_index.sort(key=lambda x: x[0], reverse=True)
+        nice_images = np.zeros((BATCH_SIZE,) + generated_images.shape[1:3], dtype=np.float32)
+        nice_images = nice_images[:, :, :, None]
+        for i in range(BATCH_SIZE):
+            idx = int(pre_with_index[i][1])
+            nice_images[i, :, :, 0] = generated_images[idx, :, :, 0]
+        image = combine_images(nice_images)
+    else:
+        noise = np.random.uniform(-1, 1, (BATCH_SIZE, 100))
+        generated_images = g.predict(noise, verbose=1)
+        image = combine_images(generated_images)
+    image = image*127.5+127.5
+    Image.fromarray(image.astype(np.uint8)).save(
+        "generated_image.png")
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", type=str)
+    parser.add_argument("--batch_size", type=int, default=128)
+    parser.add_argument("--nice", dest="nice", action="store_true")
+    parser.set_defaults(nice=False)
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = get_args()
+    if args.mode == "train":
+        train(BATCH_SIZE=args.batch_size)
+    elif args.mode == "generate":
+        generate(BATCH_SIZE=args.batch_size, nice=args.nice)
